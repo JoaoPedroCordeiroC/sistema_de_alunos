e6eefb0256ba4d7189e8a4bfe443ce1040aa5977
matheus <matheusholscher@gmail.com>
2019-04-05 22:40:08 -0300

    tab_curso

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cursos
   resources :alunos
   resources :professors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_04_06_012354) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +21,16 @@ ActiveRecord::Schema.define(version: 2019_04_06_012354) do
     t.string "email"
     t.string "pai"
     t.string "mae"
+    t.string "endereco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "professors", force: :cascade do |t|
+    t.string "nome"
+    t.string "documento"
+    t.string "email"
+    t.string "telefone"
     t.string "endereco"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
